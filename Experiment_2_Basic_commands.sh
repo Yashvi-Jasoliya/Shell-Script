76a819fe69ace4c61661aed5bbda947036f396a3
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-04-19 21:46:44 +0530

    Create Experiment_2_Basic_commands.sh

---
+++ Experiment_2_Basic_commands.sh
@@ -0,0 +1,25 @@
+Practical :- 2
+
+Command 12  :
+# AIM : Write a command to display a message hello I am learning operating system on the terminal.
+
+->  echo hello I am learning operating system on the terminal
+
+Command 13  :
+# AIM : Write a command to display comment and current time on terminal window using echo command.
+->  # This is single-line comment
+->  Date  “+Time :  %T”
+
+Command 14  :
+ # AIM : Write a command to create a directory named demo in the default directory of current user on terminal window.
+
+->  mkdir default
+->  cd default
+->  mkdir demo
+
+Command 15  :
+# AIM : Write a command to create a file named GECBVN and display the contents of it.
+
+->  touch GECBVN
+->  Hey! I am yashvi.
+->  cat ./GECBVN
