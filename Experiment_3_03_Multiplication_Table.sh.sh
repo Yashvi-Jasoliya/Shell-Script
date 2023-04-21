951dbfcd1af23c3d4d73b715fe1e62614bdcb90b
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-04-21 18:50:04 +0530

    Add files via upload

---
+++ Experiment_3_03_Multiplication_Table.sh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Taking input from user 
+
+echo "Enter the number"
+read x
+
+# printing mtable using loop
+
+echo "--> Multiplication Table of $x is :-"
+
+for((i=1; i<=10; i++))
+do
+result=$(($x*$i))
+echo "$x * $i = $result"
+done
