290162f21b00f2fcc028bf20d5adcc2883dad0ac
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-04-29 11:32:20 +0530

    Add files via upload

---
+++ Experiment_4_04_Pattern.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Enter the levels of pyramid.."
+read n
+
+echo "Pyramid of $n level :- "
+
+for((i=n; i>=1; i--))
+do
+   for((j=1; j<=n; j++))
+   do
+     if [ $j -ge $i ]
+     then 
+     echo -n "* "
+     else
+     echo -n  " "
+     fi
+  done
+echo
+done
+
+
+
