7b84a28d0c545495f05bbf82d10ada13ea6a16ca
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-05-01 00:25:56 +0530

    Add files via upload

---
+++ Experiment_3_07_Menu_Driven.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "--> Printing the menu and extecute the task"
+
+echo "Enter the number to do task :- "
+read num
+echo
+
+echo "Number $num task performing.."
+
+case $num in
+1)echo $(cal)
+;;
+2)echo $(date "+ date :- %d-%m-%y, Time :- %T")
+;; 
+3)echo $USER
+;;
+4)echo -e "\033[20;10HHello World"
+;;
+5)echo $(tty)
+;;
+*)echo Not found argument..!
+;;
+
+esac
+
+
