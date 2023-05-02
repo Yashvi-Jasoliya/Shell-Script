6447426a4de46d76f1d19b585322ecf3e9dbd84e
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-05-02 19:45:21 +0530

    Update Experiment_3_11_Grep_Egrep_Fgrep.sh

--- Experiment_3_11_Grep_Egrep_Fgrep.sh
+++ Experiment_3_11_Grep_Egrep_Fgrep.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+--> Grep, Egrep, Frep 
+echo "Enter number for perform command.."
 echo "1. Grep"
 echo "2. Egrep"
-echo "3. fgrep"
+echo "3. Fgrep"
 read n
 
 case $n in
@@ -10,14 +11,17 @@ case $n in
 read s
 grep "$s" students_name.txt
 ;;
+
 2)echo "Your Double Pettern search:- "
 read s
 read e
 egrep "$s" students_name.txt
 grep -E "$e" students_name.txt
 ;;
+
 3)echo "Your Pettern from a file :- "
 read g
-grep "$s" students_name.txt
 grep -F "$s" students_name.txt
 ;;
+
+esac
