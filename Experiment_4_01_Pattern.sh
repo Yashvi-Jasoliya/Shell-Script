81dc458efca6ad61ca8409a762f3ea5cc168632f
Yashvi Jasoliya <113770855+Yashvi-Jasoliya@users.noreply.github.com>
2023-05-08 09:59:55 +0530

    Update Experiment_4_01_Pattern.sh

--- Experiment_4_01_Pattern.sh
+++ Experiment_4_01_Pattern.sh
@@ -6,11 +6,11 @@ read n
 # rows 
 echo "Pyramid of $n level :- "
 
-for((i=n; i>=1; i--))
-do
-for((j=1; j<=i; j++))
-do
-echo -n "* "
-done
+  for((i=n; i>=1; i--))
+  do
+    for((j=1; j<=i; j++))
+    do
+    echo -n "* "
+    done
 echo ""
 done
